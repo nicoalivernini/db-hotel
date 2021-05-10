2cd11882bd0af4c936ec7716335c87b3d2694f5b
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-05-10 15:51:58 +0200

    Chiamo e stampo i dati delle stanze

--- index.php
+++ index.php
@@ -10,6 +10,27 @@
 
   //Controllo la connessione
   if ($conn && $conn->connect_error) {
-    echo "Connection failed:" . $conn->connect_error;
+    echo "Connessione fallita per:" . $conn->connect_error;
+  } else {
+    $sql = "SELECT room_number, floor FROM stanze";
+    $result = $conn->query($sql);
+
+    if ($result && $result->num_rows > 0) {
+      //Salvo i risultati in row
+      $row = $result->fetch_assoc();
+      //Ciclo i risultati
+      while ($row) {
+        //Stampo i risultati
+        echo "Stanza N." .$row['room_number']. "piano:" .$row['floor'];
+        $row = $result->fetch_assoc();
+      }
+
+    } else if ($result) {
+      echo "0 risultati";
+    } else {
+      echo "Errore nella query";
+    }
+    //Chiudo la connessione
+    $conn->close();
   }
  ?>
